14701c5a8cc596cc09c342e8da756b323ec2570d
Michael Cesare <Michael.cesare@icon.com.mt>
2020-05-10 16:25:03 +0200

    add react first code

--- src/react-app/index.js
+++ src/react-app/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Form from "./js/components/Form";
+import Form from "./components/Form";
 import logger from '../util/logger';
 
 const bootstrapApp = (renderMethod) => {
